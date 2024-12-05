84e1a949297d59b4034567a1dc6ad826c7f663a4
orbium-montez <innovo.montez@gmail.com>
2024-12-05 11:18:24 -0500

    Initial commit

---
+++ calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h> 
+
+void calculateSum{
+  int x = %d; 
+  int y = %d; 
+  int sum = x +  y;
+  printf(The sum is: %d, sum);
+}
+
+void calculateMulitplication{
+  int x = %d; 
+  int y = %d; 
+  int mult = x * y; 
+  printf(The multiplication equals: %d, mult);
+}
+
+void calculateDivision {
+  int x = %d; 
+  int y = %d; 
+  int div = x / y; 
+  printf(The division equals: %d, div);
+}
+
+voidcalculateSubtraction{
+  int x = %d; 
+  int y = %d; 
+  int sub = x - y; 
+  printf(The division equals: %d, sub)
+}
+
+int main(void){
+
+int num1; 
+int num2;
+
+printf("Hey! Welcome to this new calculator app! Please share your numbers below: \n");
+
+//take user input 
+scanf("%d %d", &num1, &num2);
+
+//ask the user which function they want to perform 
+
+printf("Enter a number for the function you want. 1.Add 2.Subtract 3.Division 4.Multiply: \n"); 
+
+//next we have the respective functions 
+
+
+
+
+
+}
+
+
+
